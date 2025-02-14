a475c68d4f6b9a860a1d14a25b303c2f0199ce0c
pageza <zap.freelance@gmail.com>
2025-02-14 22:42:52 +0000

    Working on models and clearing any redundancies

--- internal/models.go
+++ internal/models.go
@@ -25,6 +25,7 @@ type Recipe struct {
 	Title        string         `gorm:"not null" json:"title"`
 	Ingredients  string         `gorm:"type:text" json:"ingredients"`
 	Instructions string         `gorm:"type:text" json:"instructions"`
+	Calories     int            `json:"calories"`
 	UserID       uint           `gorm:"not null" json:"user_id"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
